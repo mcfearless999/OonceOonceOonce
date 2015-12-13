2e024655a9efac052c97705613f8e679a4699a84
Chris M <chrism@backup.local>
2015-12-13 15:13:19 -0800

    intro scene created

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -13,6 +13,14 @@ public class Movement : MonoBehaviour {
 	public float minhorizontal;
 	Animator myAnimator;
 
+	//restricted area check
+	//public float CheckRestricted(float x1, float x2, float y1, float y2){
+	//	int Restx = 0;
+	//	int Resty = 0;
+
+	//}
+
+
 	// Use this for initialization
 	void Start () {
 		myAnimator = this.GetComponentInChildren<Animator>();
